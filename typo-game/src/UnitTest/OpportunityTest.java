97b460d470149724bfd90b2d66cd4780dfce39fd
Thorvers <t.horvers@student.fontys.nl>
2017-09-26 15:23:23 +0200

    OpportunityTest and TestObserver

---
+++ typo-game/src/UnitTest/OpportunityTest.java
@@ -0,0 +1,52 @@
+package UnitTest;
+
+import Model.Difficulty;
+import Model.Opportunity;
+import Model.Player;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OpportunityTest{
+
+    TestObserver testObserver = new TestObserver();
+
+    Opportunity opp = new Opportunity("test", Difficulty.Beginner);
+
+    @Test
+    public void getName() throws Exception {
+        assertEquals("test",opp.getName());
+    }
+
+    @Test
+    public void setName() throws Exception {
+        opp.setName("toast");
+        assertEquals("toast",opp.getName());
+    }
+
+    @Test
+    public void getDiff() throws Exception {
+        assertEquals(Difficulty.Beginner,opp.getDiff());
+    }
+
+    @Test
+    public void setDiff() throws Exception {
+        opp.setDiff(Difficulty.Expert);
+        assertEquals(Difficulty.Expert,opp.getDiff());
+    }
+
+    @Test
+    public void getPlayer() throws Exception {
+        opp.Effect(new Player());
+        assertEquals(0,opp.getPlayer().getScore());
+    }
+
+    @Test
+    public void effect() throws Exception {
+        opp.addObserver(testObserver);
+        opp.Effect(new Player());
+
+        assertEquals(true, testObserver.success);
+    }
+
+}
---
+++ typo-game/src/UnitTest/TestObserver.java
@@ -0,0 +1,23 @@
+package UnitTest;
+
+import Model.Opportunity;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class TestObserver implements Observer {
+
+    public boolean success;
+
+    @Override
+    public void update(Observable observable, Object o) {
+
+        if (o.getClass() == Opportunity.class) {
+            Opportunity opp = (Opportunity)o;
+            success = true;
+        }
+        else {
+            success = false;
+        }
+    }
+}
