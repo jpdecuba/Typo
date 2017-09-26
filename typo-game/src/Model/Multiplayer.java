60fbbfca2e802c74c1df2abf707fe23fa9c4e72f
Scholten,Sjors S <s.scholten@student.fontys.nl>
2017-09-26 10:12:22 +0200

    session and database

---
+++ typo-game/src/Model/Multiplayer.java
@@ -0,0 +1,19 @@
+package Model;
+
+import sample.DBConnection;
+
+public class Multiplayer extends Session {
+
+    //Atributes
+    private DBConnection connection;
+
+    //Constructor
+    public Multiplayer(Difficulty difficulty){
+        //Creates a multiplayer game
+    }
+
+    @Override
+    public void Start(){
+
+    }
+}
