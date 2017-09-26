bebacc36761b580cfe65e48c303243fed85ea4a7
JDC <jeanpierre.decuba@student.fontys.nl>
2017-09-26 16:02:31 +0200

    Homepage GUI created + added logo

--- typo-game/src/Controller/Controller.java
+++ typo-game/src/Controller/Controller.java
@@ -4,9 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,16 +16,19 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
+
     @FXML
-    Button btn;
+    Button SingleplayerBtn;
+
     @FXML
     public void test(ActionEvent event) throws IOException {
         System.out.println("test");
         Stage stage;
         Parent root;
-        stage=(Stage) btn.getScene().getWindow();
+        stage=(Stage) SingleplayerBtn.getScene().getWindow();
         root = FXMLLoader.load(getClass().getResource("/Views/page2.fxml"));
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, screenSize.getWidth(), screenSize.getHeight());
         stage.setScene(scene);
         stage.show();
     }
--- typo-game/src/sample/Main.java
+++ typo-game/src/sample/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        primaryStage.getIcons().add(new Image("/Y.png"));
         Parent root = FXMLLoader.load(getClass().getResource("/Views/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("TYPO");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
