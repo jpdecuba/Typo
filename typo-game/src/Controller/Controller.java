ecf9c5ab15c0618e76ca731c16c66f71bb1ca0bf
JDC <jeanpierre.decuba@student.fontys.nl>
2017-09-26 11:09:28 +0200

    Views switch example added

--- typo-game/src/Controller/Controller.java
+++ typo-game/src/Controller/Controller.java
@@ -1,4 +1,35 @@
 package Controller;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    Button btn;
+    @FXML
+    public void test(ActionEvent event) throws IOException {
+        System.out.println("test");
+        Stage stage;
+        Parent root;
+        stage=(Stage) btn.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/Views/page2.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
