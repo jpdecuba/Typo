d334ea7cb8d8c2a2d1f6e455da487bc880f2b120
Unknown <o.olaf@hotmail.com>
2017-09-26 11:02:34 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ typo-game/src/Model/Database/Database.java
@@ -0,0 +1,37 @@
+package Model.Database;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+public class Database {
+    public static Connection connection(){
+        Connection con = null;
+        Properties properties = null;
+        try
+        {
+            InputStream input = new FileInputStream("config.properties");
+            properties.load(input);
+            
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection(properties.getProperty("path"), properties.getProperty("user"), properties.getProperty("password"));
+        }
+        catch (FileNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (ClassNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (SQLException ex)
+        {
+            ex.printStackTrace();
+        }
+        return con;
+    }
+}
