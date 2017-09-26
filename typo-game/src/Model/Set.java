925ef3eaa282a6393d5ec5157ab23364842dbc1e
Donkers,Ilona I.S.A <i.donkers@student.fontys.nl>
2017-09-26 10:44:09 +0200

    a

---
+++ typo-game/src/Model/Set.java
@@ -0,0 +1,16 @@
+package Model;
+
+public class Set {
+    private Letter[] characters;
+
+    public Set(String string){
+        characters = new Letter[string.length()];
+        for(int i = 0; i < string.length(); i++){
+            characters[i] = new Letter(string.charAt(i));
+        }
+    }
+
+    public Letter[] getCharacters() {
+        return characters;
+    }
+}
