a1d126fb3ab4a221fe30aa215008206faa93479c
Donkers,Ilona I.S.A <i.donkers@student.fontys.nl>
2017-10-03 13:57:51 +0200

    Set_Fix

--- typo-game/src/Model/Set.java
+++ typo-game/src/Model/Set.java
@@ -8,11 +8,7 @@ public class Set {
     public Set(String string){
         characters = new ArrayList<Letter>();
         for(int i = 0; i < string.length(); i++){
-<<<<<<< HEAD
-            characters[i] = new Letter(string.substring(i, 1));
-=======
-            characters.add(new Letter(string.substring(i)));
->>>>>>> da46798fc91964feabecbfa22933d985001a5a96
+            characters.add(new Letter(string.substring(i, 1)));
         }
     }
 
