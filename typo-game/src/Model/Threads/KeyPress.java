efcac0d257cc89b1bfa8a452aa8eef55c8993603
Dongen,Bart B.A.M. van <bart.van.dongen@hotmail.com>
2017-10-10 18:40:11 +0200

    Auto stash before merge of "master" and "origin/master"

--- typo-game/src/Model/Threads/KeyPress.java
+++ typo-game/src/Model/Threads/KeyPress.java
@@ -2,23 +2,34 @@ package Model.Threads;
 
 import Model.Session;
 import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+import javax.xml.stream.EventFilter;
+import javax.xml.stream.events.XMLEvent;
+import java.util.Observable;
+import java.util.Observer;
+
 import static javafx.scene.input.KeyEvent.KEY_PRESSED;
 import static javafx.scene.input.KeyEvent.KEY_RELEASED;
 
-public class KeyPress implements Runnable {
+public class KeyPress implements Runnable, Observer {
 
     private Scene scene;
 
     private Session sp;
 
+    private EventHandler keypressevent;
+
 
     public KeyPress(Scene scene, Session sp) {
         this.scene = scene;
         this.sp = sp;
+        this.sp.addObserver(this);
+
+
 
     }
 
@@ -27,41 +38,63 @@ public class KeyPress implements Runnable {
     public void run() {
 
 
-        scene.addEventFilter(KeyEvent.ANY, keyEvent -> {
-            //System.out.println(keyEvent);
 
-            String s = keyEvent.getCode().toString();
-            if (keyEvent.getCode() != KeyCode.SHIFT) {
-                if (keyEvent.getEventType() == KEY_PRESSED) {
 
-                    if (s.contains("DIGIT")) {
-                        s = s.substring(5);
+
+        keypressevent = new EventHandler<KeyEvent>(){
+
+            @Override
+            public void handle(KeyEvent keyEvent){
+
+                String s = keyEvent.getCode().toString();
+                if (keyEvent.getCode() != KeyCode.SHIFT) {
+                    if (keyEvent.getEventType() == KEY_PRESSED) {
+
+                        if (s.contains("DIGIT")) {
+                            s = s.substring(5);
+                        }
+
+
+                        if (keyEvent.isShiftDown()) {
+                            s.toUpperCase();
+                        } else {
+                            s = s.toLowerCase();
+                        }
+
+                        //char c = s.charAt(0);
+
+                        //System.out.println("key = " + s);
+
+                        typechar(s);
                     }
-
-
-                    if (keyEvent.isShiftDown()) {
-                        s.toUpperCase();
-                    } else {
-                        s = s.toLowerCase();
-                    }
-
-                    //char c = s.charAt(0);
-
-                    //System.out.println("key = " + s);
-
-                    typechar(s);
                 }
+
             }
 
-        });
+
+        };
+
+
+
+
+        scene.addEventFilter(KeyEvent.ANY, keypressevent);
+
+
 
     }
 
     public synchronized void typechar(String c) {
 
-        Platform.runLater(() -> {
+
             sp.TypeCharacter(c, sp.getPlayerOne());
-        });
+
     }
 
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+        scene.removeEventFilter(KeyEvent.ANY, keypressevent);
+        scene.removeEventHandler(KeyEvent.ANY, keypressevent);
+    }
 }
