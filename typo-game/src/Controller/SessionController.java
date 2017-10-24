5df8ad8caeb548708193e3caa9ebea0bac2c83f9
JDC <jeanpierre.decuba@student.fontys.nl>
2017-10-24 15:22:27 +0200

    added opportunity boundary

--- typo-game/src/Controller/SessionController.java
+++ typo-game/src/Controller/SessionController.java
@@ -101,6 +101,10 @@ public class SessionController implements Initializable, Observer {
         } else if (arg.getClass() == Opportunity.class) {
 
             Opp = (Opportunity) arg;
+            Opp.setMinX(0);
+            Opp.setMaxX(1000);
+            Opp.setMinY(0);
+            Opp.setMaxY(1000);
             star.setVisible(true);
             star.setX(Opp.getPosX());
             star.setY(Opp.getPosY());
