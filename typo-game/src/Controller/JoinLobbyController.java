7be68fd78a0877f985a231326f0ad4cae68a9485
JDC <jeanpierre.decuba@student.fontys.nl>
2017-12-05 14:36:33 +0100

    fixed join lobby UI

--- typo-game/src/Controller/JoinLobbyController.java
+++ typo-game/src/Controller/JoinLobbyController.java
@@ -1,15 +1,22 @@
 package Controller;
 
+import Model.Sockets.GameClient;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class JoinLobbyController implements Initializable {
+    private GameClient GC;
 
+    @FXML
+    public void btnClick() {
+
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        GC = new GameClient();
     }
 }
